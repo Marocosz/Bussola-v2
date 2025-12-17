de662691e1f78c8800574f3cca107b560e17a88c
marocosz <marcosrodriguesepro@gmail.com>
2025-12-17 08:40:27 -0300

    att font awesome

--- bussola_web/src/main.jsx
+++ bussola_web/src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+// Estilos Globais
 import './assets/styles/global.css' 
+
+// Ícones de Clima (Home)
 import 'weather-icons/css/weather-icons.css';
+
+// --- ADICIONE ESTA LINHA (Ícones Gerais) ---
+import '@fortawesome/fontawesome-free/css/all.min.css';
+// -------------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
