60c16d130bf35bcc373d422c26098721e180ed3a
marocosz <ditanixplayer@gmail.com>
2026-03-29 19:07:02 -0300

    feat(web): wrap app with ErrorBoundary

--- bussola_web/src/main.jsx
+++ bussola_web/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'; // [NOVO] Apenas este import
 import App from './App.jsx'
+import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 
 import './assets/styles/global.css' 
 import 'weather-icons/css/weather-icons.css';
@@ -12,8 +13,10 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-            <App />
-        </GoogleOAuthProvider>
+        <ErrorBoundary>
+            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+                <App />
+            </GoogleOAuthProvider>
+        </ErrorBoundary>
     </React.StrictMode>,
 )
